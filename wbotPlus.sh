e6ef78ef76734bee6e280d535300d5b4e3ce3198
vc-speed <105395710+vc-speed@users.noreply.github.com>
2022-11-30 17:01:36 -0300

    Create wbotPlus.sh

---
+++ wbotPlus.sh
@@ -0,0 +1,3 @@
+wget https://github.com/vc-speed/tools/raw/main/TBotPlus_Cliente_2008_v1.0.9.zip
+unzip TBotPlus_Cliente_*.zip && cd TBotPlus; chmod +x TerminusBot.sh && ./TerminusBot.sh
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
